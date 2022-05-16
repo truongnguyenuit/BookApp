14645106fc9d36221a16d6b03612805a66cf2679
Nguyễn Xuân Trường <truongnguyenuit@gmail.com>
2022-05-16 17:53:07 +0700

    add user infomation firebase and user infomation change function

---
+++ .history/firebase_20220516111103.js
@@ -0,0 +1,29 @@
+
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { getFires}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlOp5PyyHnkm2PDRhsHbvK8kQQ_MMprpc",
+  authDomain: "fir-rn-e9cbb.firebaseapp.com",
+  projectId: "fir-rn-e9cbb",
+  storageBucket: "fir-rn-e9cbb.appspot.com",
+  messagingSenderId: "273275229253",
+  appId: "1:273275229253:web:9d19eda69ef152856d7f2c"
+};
+
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
---
+++ .history/src/Screens/Profile_20220516171807.js
@@ -0,0 +1,170 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { Title, Card, Button } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient'
+import { MaterialIcons, Entypo, FontAwesome5 } from '@expo/vector-icons';
+import { auth, db } from '../../firebase';
+import { collection, addDoc, getDocs, doc, setDoc } from "firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { TextInput } from 'react-native-gesture-handler';
+function Profile({ navigation }) {
+  const [name, setname] = useState('null')
+  const [email, setemail] = useState('null')
+  const [phoneNumber, setphoneNumber] = useState('null')
+  const [address, setaddress] = useState('null')
+
+  const handleSubmit = async () => {
+    const citiesRef = collection(db, "users");
+
+    await setDoc(doc(citiesRef, userUID), {
+      Name: name,
+      Email: email,
+      Phonenumber: phoneNumber,
+      Address: address,
+      UID: userUID,
+    });
+
+
+
+  }
+  const handleReadDB = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+
+    querySnapshot.forEach((doc) => {
+      //console.warn(`${doc.id} => ${doc.data(UID)}`);
+    if (`${doc.id}`== userUID) {
+      const { Address, Email, Name, Phonenumber } = doc.data();
+      alert( a)
+    }
+      
+
+    });
+
+  }
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("SignInScreen")
+
+      })
+      .catch(error => alert(error.message))
+  }
+  var userUID;
+  var user = firebase.auth().currentUser;
+
+  if (user)
+    userUID = user.uid
+  return (
+
+    <View style={styles.root}>
+      <LinearGradient
+        colors={['#ff8c00', 'orange']}
+        style={{ height: '20%' }}
+      >
+        <View style={styles.header}>
+          <Text style={{ fontSize: 38, color: "#2d3436" }}>Personal <Text style={{ fontWeight: '300', color: "white" }}>Profile</Text></Text>
+        </View>
+      </LinearGradient>
+      <View style={styles.imagestyle}>
+        <Image
+          style={{ width: 140, height: 140, borderRadius: 70, margin: -50 }}
+          source={{ uri: "https://images.unsplash.com/photo-1563235453-a57d94b5a552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" }}
+        />
+      </View>
+      <View style={{ alignItems: "center", marginTop: 55, margin: 15 }}>
+        <TextInput
+          placeholder={name}
+          style={styles.titlename}
+          value={name}
+          onChangeText={text => setname(text)}
+        >
+
+        </TextInput>
+      </View>
+      <Card style={styles.mycard}>
+        <View style={styles.cardcontainer}>
+          <MaterialIcons style={{ margin: 4 }} name="email" size={32} color='#ff8c00' />
+          <TextInput
+            style={{ marginTop: 10, fontSize: 15 }}
+            value={email}
+            onChangeText={text => setemail(text)}
+          ></TextInput>
+        </View>
+
+      </Card>
+      <Card style={styles.mycard} >
+        <View style={styles.cardcontainer}>
+          <Entypo style={{ margin: 4 }} name="phone" size={32} color='#FF8C00' />
+          <TextInput
+            style={{ marginTop: 10, fontSize: 15 }}
+            value={phoneNumber}
+            onChangeText={text => setphoneNumber(text)}
+          ></TextInput>
+        </View>
+      </Card>
+
+      <Card style={styles.mycard} >
+        <View style={styles.cardcontainer}>
+          <FontAwesome5 style={{ margin: 4 }} name="home" size={24} color='#FF8C00' />
+          <TextInput
+            style={{ marginTop: 10, fontSize: 15 }}
+            value={address}
+            onChangeText={text => setaddress(text)}
+          ></TextInput>
+        </View>
+      </Card>
+
+      <Card style={styles.mycard} >
+        <View style={styles.cardcontainer}>
+
+          <Text style={{ marginTop: 12, fontSize: 15 }}> UserID: {userUID}</Text>
+        </View>
+      </Card>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15, color: '#FF8C00' }}>
+        <Button icon="account-edit" color="#FF8C00" mode="contained" onPress={handleSubmit}>
+          Edit
+        </Button>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15, color: '#FF8C00' }}>
+        <Button color="#FF8C00" mode="contained" onPress={handleSignOut}>
+          SIGN OUT
+        </Button>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  imagestyle: {
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  mycard: {
+    margin: 3,
+    marginTop: 10,
+  },
+  cardcontainer: {
+    flexDirection: 'row',
+    padding: 5
+  },
+  titlename: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
+
+
+})
+export default Profile;
