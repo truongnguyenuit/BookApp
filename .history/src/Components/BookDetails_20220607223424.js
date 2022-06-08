866d39002b1c0eea95d4ae10b26f88df38750bd4
Nguyễn Xuân Trường <truongnguyenuit@gmail.com>
2022-06-08 13:39:51 +0700

    Fixbug and complete Comment function

---
+++ .history/src/Components/BookDetails_20220607223424.js
@@ -0,0 +1,372 @@
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View, Dimensions, TouchableOpacity, ScrollView, TextInput } from "react-native";
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import LikeButton from '../Components/LikeButton';
+const screen_width = Dimensions.get('window').width;
+const screen_height = Dimensions.get('window').height;
+import { LinearGradient } from 'expo-linear-gradient';
+import StarRating from '../Components/StarRating'
+
+import { db } from '../../firebase'
+import { collection, addDoc, getDocs, doc, setDoc, query, where } from "firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+export default function BookDetails({ route, navigation }) {
+  const { Img, Name, Author, Description, Content } = route.params
+
+  const handleLike = async () => {
+    const citiesRef = collection(db, "likes");
+    await setDoc(doc(citiesRef, Name), {
+      img: Img,
+      name: Name,
+      author: Author,
+      description: Description,
+      content: Content,
+      key: Name,
+      UID: userUID,
+    });
+  }
+
+  const [comment, setComment] = useState()
+  const handleComment = async () => {
+    const citiesRef = collection(db, "comments");
+    await setDoc(doc(citiesRef), {
+      name: Name,
+      content: comment,
+      userName: name,
+    });
+  }
+
+  const [name, setName] = useState()
+  const HandleReadDB = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      // console.warn(`${doc.id}`);
+      console.log('1')
+      if (`${doc.id}` == userUID) {
+        console.log('2')
+        const { Name } = doc.data();
+        setName(Name)
+      }
+    });
+  }
+  const [ccontent, setCcontent] = useState()
+  const [nname, setNname] = useState()
+  const [uuserName, setUuserName] = useState()
+  const [arr, setarr] = useState([]);
+
+  const ReadCommentFromDB = async () => {
+    const q = query(collection(db, "comments"), where("name", "==", Name))
+    const querySnapshot = await getDocs(q);
+
+    setarr([])
+
+    querySnapshot.forEach((doc) => {
+      console.log('3')
+      const { content, name, userName } = doc.data();
+      setCcontent(content)
+      setNname(name)
+      setUuserName(userName)
+
+      setarr(oldArray => [...oldArray, { cccontent: ccontent, nnname: nname, uuusername: uuserName }])
+    });
+  }
+
+  useEffect(() => {
+    HandleReadDB()
+    ReadCommentFromDB()
+  }, []);
+
+  var userUID;
+  var user = firebase.auth().currentUser;
+  if (user)
+    userUID = user.uid
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+      <TouchableOpacity 
+        style={styles.card}
+        onPress={()=> navigation.navigate("DetailsCategory",{categoryy:"Funny", navigation: navigation})}
+        >
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/funny.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Funny Category</Text>
+            <StarRating ratings={4} reviews={99} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this category
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={styles.card}
+        onPress={()=> navigation.navigate("DetailsCategory",{categoryy: "Drama", navigation: navigation})}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/drama.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Drama Category</Text>
+            <StarRating ratings={5} reviews={150} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/romantic.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Romantic Category</Text>
+            <StarRating ratings={3} reviews={50} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/action.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Action Category</Text>
+            <StarRating ratings={2} reviews={20} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/horror.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Horror Category</Text>
+            <StarRating ratings={4} reviews={110} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/scienceFiction.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Science Fiction Category</Text>
+            <StarRating ratings={2} reviews={20} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../Screens/Images/scienceFiction.jpg')}
+              resizeMode="stretch"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Science Fiction Category</Text>
+            <StarRating ratings={2} reviews={20} />
+            <Text style={styles.cardDetails}>
+              Amazing description for this Category
+            </Text>
+          </View>
+        </TouchableOpacity>
+      {/* <View style={styles.test}>
+        <Text>asdasd</Text>
+      </View>
+        <View style={styles.top}>
+          <Image source={{ url: Img }} style={styles.coverImg}></Image>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={handleLike}>
+            <LikeButton />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.bot}>
+          <View style={styles.textview}>
+            <Text style={styles.header}>{Name}</Text>
+            <Text style={styles.author}>By: {Author}</Text>
+            <Text style={styles.title}>Description:</Text>
+            <Text style={styles.description}>{Description}</Text>
+          </View>
+          <Text style={styles.textComment}> Comment:</Text>
+
+          <View style={styles.cardcontainer}>
+            <TextInput
+              style={styles.comment}
+              value={comment}
+              placeholder={'Your comment'}
+              onChangeText={text => setComment(text)}
+            >
+            </TextInput>
+            <TouchableOpacity
+              style={styles.commentbutton}
+              onPress={handleComment}
+            >
+              <Text style={styles.buttonText}>Comment</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.commentContainer}>
+            {arr.map((comment, key) => (
+                <View>
+                  <Text>asdasd</Text>
+                </View>
+            ))}
+          </View>
+          <View style={styles.buttoncontainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate('Home')}
+            >
+
+              <Text style={styles.buttontext}>
+                Go Home
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate('BookContent', { img: Img, name: Name, author: Author, content: Content })}
+            >
+              <Text style={styles.buttontext}>
+                Start Read
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+        </View> */}
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+  container: {
+   
+  },
+
+  // top: {
+  //   height: "90%",
+  //   width: "100%"
+  // },
+  // bot: {
+  //   height: "50%",
+  //   width: "100%"
+  // },
+  // coverImg: {
+  //   width: "100%",
+  //   height: "100%",
+  //   resizeMode: "stretch"
+  // },
+  // textview: {
+  //   margin: 10
+  // },
+  // header: {
+  //   fontWeight: 'bold',
+  //   fontSize: 20
+  // },
+  // author: {
+  //   marginVertical: 10,
+  // },
+  // title: {
+  //   fontWeight: 'bold',
+  //   fontSize: 20,
+  // },
+  // description: {
+  //   marginVertical: 10,
+  // },
+  // buttoncontainer: {
+  //   flex: 1,
+  //   flexDirection: 'row',
+  //   justifyContent: 'space-between',
+  //   alignItems: 'center',
+  //   paddingHorizontal: 20,
+
+  // },
+  // button: {
+  //   backgroundColor: '#FF8C00',
+  //   padding: 10,
+  //   borderRadius: 50,
+  //   paddingHorizontal: 30
+
+  // },
+  // buttontext: {
+  //   fontWeight: 'bold',
+  //   color: 'white'
+  // },
+  // btn: {
+  //   position: 'absolute',
+  //   marginTop: screen_height - 450,
+  //   marginLeft: screen_width - 80,
+  // },
+  // cardcontainer: {
+  //   justifyContent: 'space-between',
+  //   flexDirection: 'row',
+  //   padding: 10,
+  // },
+  // textComment: {
+  //   fontWeight: 'bold',
+  //   fontSize: 17,
+  //   marginLeft: 10,
+  // },
+  // comment: {
+
+  //   borderWidth: 2,
+  //   padding: 10,
+  //   borderRadius: 10,
+  //   borderColor: '#ff8c00',
+  //   width: 292,
+  // },
+  // commentbutton: {
+  //   backgroundColor: '#ff8c00',
+  //   justifyContent: 'center',
+  //   borderRadius: 10,
+  //   paddingHorizontal: 10,
+  // },
+  // buttonText: {
+  //   color: 'white',
+  //   fontWeight: 'bold',
+  // }
+
+
+})
---
+++ .history/src/Components/BookDetails_20220608102403.js
@@ -0,0 +1,267 @@
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View, Dimensions, TouchableOpacity, ScrollView, TextInput } from "react-native";
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import LikeButton from '../Components/LikeButton';
+const screen_width = Dimensions.get('window').width;
+const screen_height = Dimensions.get('window').height;
+import { LinearGradient } from 'expo-linear-gradient';
+import StarRating from './StarRating'
+
+import { db } from '../../firebase'
+import { collection, addDoc, getDocs, doc, setDoc, query, where } from "firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+export default function BookDetails({ route, navigation }) {
+  const { Img, Name, Author, Description, Content } = route.params
+
+  const handleLike = async () => {
+    const citiesRef = collection(db, "likes");
+    await setDoc(doc(citiesRef, Name), {
+      img: Img,
+      name: Name,
+      author: Author,
+      description: Description,
+      content: Content,
+      key: Name,
+      UID: userUID,
+    });
+  }
+
+  const [comment, setComment] = useState()
+  const handleComment = async () => {
+    const citiesRef = collection(db, "comments");
+    await setDoc(doc(citiesRef), {
+      name: Name,
+      content: comment,
+      userName: name,
+    });
+  }
+
+  const [name, setName] = useState()
+  const HandleReadDB = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      // console.warn(`${doc.id}`);
+      console.log('1')
+      if (`${doc.id}` == userUID) {
+        console.log('2')
+        const { Name } = doc.data();
+        setName(Name)
+      }
+    });
+  }
+  const [ccontent, setCcontent] = useState()
+  const [nname, setNname] = useState()
+  const [uuserName, setUuserName] = useState()
+  const [arr, setarr] = useState([]);
+
+  const ReadCommentFromDB = async () => {
+    const q = query(collection(db, "comments"), where("name", "==", Name))
+    const querySnapshot = await getDocs(q);
+
+    setarr([])
+
+    querySnapshot.forEach((doc) => {
+      console.log('3')
+      const { content, name, userName } = doc.data();
+      setCcontent(content)
+      setNname(name)
+      setUuserName(userName)
+
+      setarr(oldArray => [...oldArray, { cccontent: ccontent, nnname: nname, uuusername: uuserName }])
+    });
+  }
+
+  useEffect(() => {
+    HandleReadDB()
+    ReadCommentFromDB()
+  }, []);
+
+  var userUID;
+  var user = firebase.auth().currentUser;
+  if (user)
+    userUID = user.uid
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+
+        <View style={styles.imgContainer}>
+          <Image
+            source={{ url: Img }}
+            resizeMode='stretch'
+            style={styles.coverImg}
+          >
+          </Image>
+        </View>
+
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={handleLike}>
+          <LikeButton />
+        </TouchableOpacity>
+
+        <View style={styles.textview}>
+          <Text style={styles.header}>{Name}</Text>
+          <Text style={styles.author}>By: {Author}</Text>
+          <Text style={styles.title}>Description:</Text>
+          <Text style={styles.description}>{Description}</Text>
+        </View>
+        <View style={styles.buttoncontainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Home')}
+          >
+
+            <Text style={styles.buttontext}>
+              Go Home
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('BookContent', { img: Img, name: Name, author: Author, content: Content })}
+          >
+            <Text style={styles.buttontext}>
+              Start Read
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.textComment}> Comment:</Text>
+
+        <View style={styles.cardcontainer}>
+          <TextInput
+            style={styles.comment}
+            value={comment}
+            placeholder={'Your comment'}
+            onChangeText={text => setComment(text)}
+          >
+          </TextInput>
+          <TouchableOpacity
+            style={styles.commentbutton}
+            onPress={handleComment}
+          >
+            <Text style={styles.buttonText}>Comment</Text>
+          </TouchableOpacity>
+        </View>
+
+
+
+
+        <View style={styles.commentContainer}>
+          {arr.map((comment, key) => (
+            <View
+              style={styles.userComment}
+              key={key}
+            >
+              <Text>{comment.}</Text>
+            </View>
+          ))}
+        </View>
+
+
+
+
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+
+  },
+  imgContainer: {
+    height: 300,
+  },
+  // top: {
+  //   flex: 1,
+  //   height: "90%",
+  //   width: "100%"
+  // },
+  // bot: {
+  //   height: "50%",
+  //   width: "100%"
+  // },
+  coverImg: {
+    width: "100%",
+    height: "100%",
+    alignSelf: 'center',
+
+  },
+  textview: {
+    margin: 10
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  author: {
+    marginVertical: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  description: {
+    marginVertical: 10,
+  },
+  buttoncontainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+
+  },
+  button: {
+    backgroundColor: '#FF8C00',
+    padding: 10,
+    borderRadius: 50,
+    paddingHorizontal: 30
+
+  },
+  buttontext: {
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  btn: {
+    position: 'absolute',
+    marginTop: screen_height - 480,
+    marginLeft: screen_width - 80,
+  },
+  cardcontainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 10,
+  },
+  textComment: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginLeft: 10,
+  },
+  comment: {
+
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: '#ff8c00',
+    width: 292,
+  },
+  commentbutton: {
+    backgroundColor: '#ff8c00',
+    justifyContent: 'center',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+
+
+})
